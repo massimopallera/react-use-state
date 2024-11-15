991f9e5435b8d95fa4f73069a9e986d890f78ea2
massimopallera <pallera.massimo@gmail.com>
2024-11-15 14:19:00 +0100

    created static structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ import './App.css'
 function App() {
   return (
     <>
-      
+      <header><h1>Learn Web Development</h1></header>
+
+      <div className="container">
+        <div className="accordion">
+
+          <div className="accordion-item">
+            <button>HTML</button>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo repellendus, porro esse, itaque consequuntur enim ipsam animi facere quod maiores omnis fugit quos tempore, rerum dolorum iste saepe! Aperiam, qui.</p>
+          </div>
+
+        </div>
+      </div>
     </>
   )
 }
