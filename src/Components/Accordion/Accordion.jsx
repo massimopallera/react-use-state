20c6821684a075b1be3ded1852d1535de3177ba4
massimopallera <pallera.massimo@gmail.com>
2024-11-17 18:16:32 +0100

    created Accordion component

---
+++ src/Components/Accordion/Accordion.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import lang from "../../assets/db/languages"
+export default function Accordion() {
+  
+  const [active, setActive] = useState(-1)
+
+  let description 
+  let descClass
+  if (active == -1) {
+    description = 'Nessun linguaggio selezionato'
+    descClass = 'choose-language'
+  } else {
+    description = lang[active].description
+    descClass = lang[active].slug.toLowerCase() 
+  }
+
+
+  function handleClick(e) {
+    const newActive = e.target.getAttribute('data-index')
+    console.log(newActive);
+    setActive(newActive)
+  }
+
+  return (
+    <>
+    <div className="accordion">
+      {lang.map((element, index) => (
+        <>
+          <div className={`accordion-item ${active == index ? 'active' : 'hide'}`}  key={index}>
+            <button data-index={index} onClick={handleClick} className={element.slug.toLowerCase()}>{element.title}</button>
+          </div>
+        </>
+      ))}
+    </div>
+
+    <p className={descClass}>{description}</p>
+      </>
+  )
+}
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,45 +1,16 @@
 import { useState } from "react"
 import lang from "../assets/db/languages"
+import Accordion from "./Accordion/Accordion"
 
 export default function Main() {
     
-  const [active, setActive] = useState(-1)
-
-  let description 
-  let descClass
-  if (active == -1) {
-    description = 'Nessun linguaggio selezionato'
-    descClass = 'choose-language'
-  } else {
-    description = lang[active].description
-    descClass = lang[active].slug.toLowerCase() 
-  }
 
 
-  function handleClick(e) {
-    const newActive = e.target.getAttribute('data-index')
-    console.log(newActive);
+  return (
     
+    <div className="container">
 
-    setActive(newActive)
-  }
-
-  return (<div className="container">
-        <div className="accordion">
-
-          
-          {lang.map((element, index) => (
-            <>
-              <div className={`accordion-item ${active == index ? 'active' : 'hide'}`}  key={index}>
-                <button data-index={index} onClick={handleClick} className={element.slug.toLowerCase()}>{element.title}</button>
-              </div>
-            </>
-            ))
-          }
-
-
-
-        </div>
-          <p className={descClass}>{description}</p>
-      </div>)
+      {/* ACCORDION */}
+      <Accordion />
+    </div>)
 }
